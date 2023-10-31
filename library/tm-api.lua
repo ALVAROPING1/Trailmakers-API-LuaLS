0b77980d79cdc0b6f3a616a000ec49eb44caaca6
ALVAROPING1 <alvaroping1@gmail.com>
2023-10-31 15:45:14 +0100

    added note about letters being case insensitive for keybind callback functions

--- library/tm-api.lua
+++ library/tm-api.lua
@@ -673,6 +673,8 @@ function tm.audio.GetAudioNames() end
 
 ---Keys that can have callbacks registered
 ---
+---Note: letters are case insensitive
+---
 ---[View documents](https://flashbulb.atlassian.net/wiki/spaces/TMMOD/pages/218267762/Input#List-of-key-names)
 ---@alias InputKey "`" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "0" | "-" | "=" | "a" | "b" | "c" | "d" | "e" | "f" | "g" | "h" | "i" | "j" | "k" | "l" | "m" | "n" | "o" | "p" | "q" | "r" | "s" | "t" | "u" | "v" | "w" | "x" | "y" | "z" | "A" | "B" | "C" | "D" | "E" | "F" | "G" | "H" | "I" | "J" | "K" | "L" | "M" | "N" | "O" | "P" | "Q" | "R" | "S" | "T" | "U" | "V" | "W" | "X" | "Y" | "Z" | "[" | "]" | ";" | "'" | "\\" | "," | "." | "/" | "backspace" | "tab" | "enter" | "left shift" | "right shift" | "left control" | "left alt" | "space" | "right alt" | "right control" | "insert" | "home" | "page up" | "delete" | "end" | "page down" | "up" | "down" | "left" | "right" | "numlock" | "[/]" | "[*]" | "[-]" | "[+]" | "[enter]" | "[,]" | "[1]" | "[2]" | "[3]" | "[4]" | "[5]" | "[6]" | "[7]" | "[8]" | "[9]" | "[0]"
 
